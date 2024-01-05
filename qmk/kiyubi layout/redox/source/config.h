54eb28042a4a790129816bd05d9842e2d88e3a8d
Bruno Madeira <bmad.works@gmail.com>
2024-01-05 01:15:28 +0000

    time to bite the bullet; it was meant to be.

--- qmk/kiyubi layout/redox/source/config.h
+++ qmk/kiyubi layout/redox/source/config.h
@@ -3,6 +3,7 @@
 
 //this is required for the shift mod
 #define ONESHOT_TAP_TOGGLE 2  // Tapping this number of times holds the key until tapped once again.
+#define TAPPING_TERM 100
 
 #undef ONESHOT_TIMEOUT
 #define ONESHOT_TIMEOUT 1000  // Time (in ms) before the one shot key is released
