e8806bb6133f6806c820f84c963d0f9bf0f2e345
Bruno Madeira <bmad.works@gmail.com>
2024-01-05 01:23:21 +0000

    ****, forgot tapping term also affects tap dance... I think this is the only way; now I need to set them short for the OSM keys.

--- qmk/kiyubi layout/redox/source/config.h
+++ qmk/kiyubi layout/redox/source/config.h
@@ -3,7 +3,7 @@
 
 //this is required for the shift mod
 #define ONESHOT_TAP_TOGGLE 2  // Tapping this number of times holds the key until tapped once again.
-#define TAPPING_TERM 100
+#define TAPPING_TERM_PER_KEY  
 
 #undef ONESHOT_TIMEOUT
 #define ONESHOT_TIMEOUT 1000  // Time (in ms) before the one shot key is released
